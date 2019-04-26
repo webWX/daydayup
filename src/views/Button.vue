1cc264b54907762be0384be72bd8f411ff676693
wangxiong <xiong.wang@dolabank.com>
2019-04-26 13:44:31 +0800

    css--button

---
+++ src/views/Button.vue
@@ -0,0 +1,20 @@
+<template>
+    <section class="button__container">
+        <h3>button</h3>
+        <div>
+            <a href="javascript:;" class="button">按钮</a>
+            <button href="javascript:;" class="button active">按钮选中</button>
+            <a href="javascript:;" class="button disabled">按钮禁用</a>
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: "Button"
+    };
+</script>
+
+<style scoped>
+
+</style>
